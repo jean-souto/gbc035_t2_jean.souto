c4ceaa4e781e8d9c445a0a7354cd22f852e58fde
Jean Souto <jeansouto123@gmail.com>
2023-08-24 12:07:25 -0300

    Update ex7.java

--- LAB01 - Procedural/ex03/res03/src/ex7.java
+++ LAB01 - Procedural/ex03/res03/src/ex7.java
@@ -1,6 +1,37 @@
+import java.util.Scanner;
+
 public class ex7 {
 
     public static void main (String[] args) {
-        int[] alunos = new int[5];
+        float[] alunos = new float[5];
+        float nun = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite as notas dos alunos: ");
+
+        for (int i = 0; i < 5; i++)
+        {
+            alunos[i] = scan.nextFloat();
+        }
+
+        nun = alunos[0];
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (alunos[i] > nun) {
+                nun = alunos[i];
+            }
+        }
+
+        nun = 100/nun;
+
+        System.out.println("As notas normalizadas sao: ");
+
+        for (int i = 0; i < 5; i++)
+        {
+            alunos[i] = alunos[i]*nun;
+            System.out.println(alunos[i]);
+        }
     }
 }
