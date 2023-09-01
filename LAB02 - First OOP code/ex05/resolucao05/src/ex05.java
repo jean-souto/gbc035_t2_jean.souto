2308ca5f45a6d7465f1576c7fb3c145db25b52cf
Jean Souto <jeansouto123@gmail.com>
2023-08-31 21:16:00 -0300

    Create ex05.java

---
+++ LAB02 - First OOP code/ex05/resolucao05/src/ex05.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ex05 {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner (System.in);
+        selecao teste = new selecao(("socorro"), ("jorge"), ("brasil"));
+
+        teste.mostrarselecao(teste);
+    }
+}
