c33da437eeac84f0dd7b2365c466ce91b67d9c8f
Jean Souto <jeansouto123@gmail.com>
2023-09-18 16:44:57 -0300

    Update ex03.java

--- LAB03 - Class Relations/resolucao03/src/ex03.java
+++ LAB03 - Class Relations/resolucao03/src/ex03.java
@@ -1,9 +1,6 @@
 public class ex03 {
     public static void main (String[] args) {
-        setor setores = new setor(("Calçados"), ("Tênis"), (150));
-        loja loja = new loja(("Lourdes Lojas"), ("Geral"), (setores));
-
-        setores.limparSetor();
+        loja loja = new loja(("Lourdes Lojas"), ("Geral"), ("Calçados"), (150), ("Tênis"));
 
         System.out.println("Loja: \n");
         loja.mostrarLoja();
