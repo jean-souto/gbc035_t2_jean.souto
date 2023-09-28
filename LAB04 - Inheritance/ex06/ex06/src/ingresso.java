cf47e60de7540753a8af67cd9ad9d57dba2f3cee
Jean Souto <jeansouto123@gmail.com>
2023-09-28 10:38:07 -0300

    Create ingresso.java

---
+++ LAB04 - Inheritance/ex06/ex06/src/ingresso.java
@@ -0,0 +1,14 @@
+public class ingresso {
+    protected double valor;
+
+    public ingresso () {
+    }
+
+    public void escreveValor (double valor) {
+        this.valor = valor;
+    }
+
+    public double retornaValor () {
+        return valor;
+    }
+}
