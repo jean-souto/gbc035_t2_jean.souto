c24fa55107b101e9cf2fb66a49cea0d33f04dc20
Jean Souto <jeansouto123@gmail.com>
2023-09-28 10:38:36 -0300

    Create operario.java

---
+++ LAB04 - Inheritance/ex05/ex05/src/operario.java
@@ -0,0 +1,31 @@
+public class operario extends funcionario {
+    private double producao;
+    private double quantProd;
+
+    public operario() {
+    }
+
+    public operario(String nome, String data_nasc) {
+        this.nome = nome;
+        this.data_nasc = data_nasc;
+        this.salario = 0;
+    }
+
+    public void alterarSalario(double producao, double quantProd) {
+        this.salario = producao*quantProd;
+        this.producao = producao;
+        this.quantProd = quantProd;
+    }
+
+    public double retornarSalario() {
+        return salario;
+    }
+
+    public void mostrar_operario () {
+        System.out.println("\nOperario: ");
+        System.out.println("Nome: " + nome);
+        System.out.println("Data de nascimento: " + data_nasc);
+        System.out.println("Valor por prod e quant última prod: " + producao + ", " + producao);
+        System.out.println("Último salário recebido: " + salario);
+    }
+}
