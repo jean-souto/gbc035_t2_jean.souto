af81edb97d98c31c4e331c674752c1627f7cc460
Jean Souto <jeansouto123@gmail.com>
2023-09-28 10:38:04 -0300

    Create ingressoNormal.java

---
+++ LAB04 - Inheritance/ex06/ex06/src/ingressoNormal.java
@@ -0,0 +1,12 @@
+public class ingressoNormal extends ingresso {
+    public ingressoNormal () {
+    }
+
+    public ingressoNormal (double valor) {
+        this.valor = valor;
+    }
+
+    public void falarIngresso () {
+        System.out.println("Ingresso normal");
+    }
+}
