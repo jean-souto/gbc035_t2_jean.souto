6412b89e67735e8fc90ce3a4548c61faaf86e3db
Jean Souto <jeansouto123@gmail.com>
2023-11-13 23:37:19 -0300

    Create formaTridimensional.java

---
+++ LAB05 - Polymorphism/ex02/ex02/src/formaTridimensional.java
@@ -0,0 +1,16 @@
+public class formaTridimensional extends forma {
+    public formaTridimensional(int arestas) {
+        super(arestas);
+    }
+
+    public formaTridimensional() {
+    }
+
+    public double obterArea () {
+        return 0;
+    }
+
+    public double obterVolume () {
+        return 0;
+    }
+}
