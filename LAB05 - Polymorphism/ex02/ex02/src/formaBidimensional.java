d67ea39ac59a2c4035efc6ef439fe25055109f43
Jean Souto <jeansouto123@gmail.com>
2023-11-13 23:37:25 -0300

    Create formaBidimensional.java

---
+++ LAB05 - Polymorphism/ex02/ex02/src/formaBidimensional.java
@@ -0,0 +1,13 @@
+public class formaBidimensional extends forma {
+
+    public formaBidimensional(int arestas) {
+        super(arestas);
+    }
+
+    public formaBidimensional() {
+    }
+
+    public double obterArea () {
+        return 0;
+    }
+}
